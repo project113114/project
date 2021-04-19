3cb1615e305dd5b194f45a339d01015b70052752
mbat113114 <77671050+mbat113114@users.noreply.github.com>
2021-04-19 10:13:46 +0530

    Update python file

--- project_main.py
+++ project_main.py
@@ -1,1 +1,54 @@
-print("project")
+
+
+import random
+import colorama
+from colorama import Fore,Back,Style
+im
+colorama.init(autoreset = True)
+#class 
+
+
+def dec1(fun):
+  def exc():
+    print(f"{Fore.GREEN} excuting now")
+    fun()
+    print(f"{Fore.GREEN} excuted")
+  return exc
+
+
+
+#class
+
+
+
+
+
+
+title = f"{Fore.GREEN}W" + f"{Fore.RED}E" + f"{Fore.BLACK}L" + f"{Fore.BLUE}C" + f"{Fore.WHITE}O" + f"{Fore.GREEN}M" + f"{Fore.RED}E"
+
+    
+
+print(Style.BRIGHT + title)
+print(Fore.GREEN + "option are ;")
+print(f"{Fore.GREEN} 01 page 1 ")
+print(f"{Fore.GREEN} 02 page 2 ")
+print(f"{Fore.GREEN} 03 page 3 ")
+print(f"{Fore.GREEN} 04 page 4 ")
+print()
+x = int(input(f"{Fore.BLUE}select the option "))
+
+
+@dec1
+def pro():
+    if x== 1:
+        print(f"{Fore.GREEN} option is ", x)
+    if  x== 2:
+        print(f"{Fore.GREEN} option is ", x)
+    if  x== 3:
+        print(f"{Fore.GREEN} option is ", x)
+    if  x== 4:
+        print(f"{Fore.GREEN} option is ", x)
+
+    else:
+    	print(f"{Fore.RED}{Style.BRIGHT}enter vaild option")
+pro()  
