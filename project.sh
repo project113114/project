dd806a27c5acaf7578b38659663d44192f4f1acc
project113114 <82789883+project113114@users.noreply.github.com>
2021-04-19 15:47:30 +0530

    update project.sh

--- project.sh
+++ project.sh
@@ -24,13 +24,13 @@ if ["$arg" == "1"]
 then
   echo "option is one "
   $ python project_main.py
-elif ["$arg" == "2"]
+elif [$arg == 2]
 then
   echo "option is two "
-elif ["$arg" == "3"]
+elif [$arg == 3]
 then
   echo "option os three"
-elif ["$arg" == "4"]
+elif [$arg == 4]
 then 
   echo "option is four"
 else 
