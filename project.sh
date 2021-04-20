d98e8411e7e04191a323d26854cb9eda99e798ef
project113114 <82789883+project113114@users.noreply.github.com>
2021-04-20 10:44:00 +0530

    update project.sh

--- project.sh
+++ project.sh
@@ -20,19 +20,19 @@ echo "(03) for 3rd "
 echo "(04) for 4th "
 
 read -p "Enter the option " arg 
-if ["$arg" == "1"]
+if ["$arg" == "1"];
 then
-  echo "option is one "
-  $ python project_main.py
-elif [$arg == 2]
+    echo "option is one "
+    $ python project_main.py
+elif [$arg == 2];
 then
-  echo "option is two "
-elif [$arg == 3]
+    echo "option is two "
+elif [$arg == 3];
 then
-  echo "option os three"
-elif [$arg == 4]
+    echo "option os three"
+elif [$arg == 4];
 then 
-  echo "option is four"
+    echo "option is four"
 else 
-  echo "enter the correct option "
+    echo "enter the correct option "
 fi
