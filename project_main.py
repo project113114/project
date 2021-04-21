a308d3c09c3946be263d2c26d9ec2a6185f55ebd
project113114 <82789883+project113114@users.noreply.github.com>
2021-04-21 20:22:38 +0530

    update project_main.py

--- project_main.py
+++ project_main.py
@@ -37,3 +37,23 @@ print(logo_project)
 
 # some options for opration 
 print(f"{Fore.RED}OPTIONS"+ f"{Fore.GREEN} for oprations ")
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
+
+print(message.sid)
+
