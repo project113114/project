76169aedaccf1ae6203c1f94024162c666460038
project113114 <82789883+project113114@users.noreply.github.com>
2021-04-21 21:48:21 +0530

    update project_main.py

--- project_main.py
+++ project_main.py
@@ -40,9 +40,8 @@ print(logo_project)
 print(f"{Fore.RED}OPTIONS"+ f"{Fore.GREEN} for oprations ")
 #option 
 print("1 "+f"{fore.RED}Custom sms service ")
-print(f"{Fore.GREEN}default country code +91")
-x = input("enter number : ")
-m = input("enter message :")
+op = int(input("choose option :"))
+
 # Download the helper library from https://www.twilio.com/docs/python/install
 
 
@@ -53,8 +52,15 @@ account_sid = 'ACdb26d19a478a3dfef65fbe4b0bb42f68'
 auth_token = 'e99c7212ea1250bf71be657923a9eb2c' 
 client = Client(account_sid, auth_token) 
  
+if(op == 1):
+  print(f"{Fore.GREEN}default country code +91")
+  x = input(f"{Fore.BLUE}enter number : ")
+  m = input(f"{Fore.GREEN}enter message :")
+  multi = int(input(f"{Fore.RED}enter nnumber of time to send message :"))
+  ma  
 
-def m():
+
+def ma():
   message = client.messages.create(  
                               messaging_service_sid='MGe1e8f1dc8aa402cc4f3e99193f8b4819', 
                               body=m,      
